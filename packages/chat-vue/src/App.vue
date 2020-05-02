5da2a7e57d5fcf53e7095615623f9573393c4048
Holayn <kwwong1597@gmail.com>
2020-05-02 17:59:22 -0400

    chore(vuetify): add

--- packages/chat-vue/src/App.vue
+++ packages/chat-vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
+  <v-app>
     <router-view/>
-  </div>
+  </v-app>
 </template>
 
 <script lang="ts">
@@ -16,22 +16,4 @@ export default class App extends Vue {
 
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<style lang="scss"></style>

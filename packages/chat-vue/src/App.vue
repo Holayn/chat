8bd87371952fa4c85a7b0a0185890cc6a7085315
Holayn <kwwong1597@gmail.com>
2020-08-16 16:52:23 -0400

    feat(socket): add dep

--- packages/chat-vue/src/App.vue
+++ packages/chat-vue/src/App.vue
@@ -9,10 +9,7 @@
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
 
-@Component({
-  components: {},
-})
-
+@Component({})
 export default class App extends Vue {
   @Watch('hasUser', {immediate: true})
   public onHasUserUpdated(hasUser: boolean) {

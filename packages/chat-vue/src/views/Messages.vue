02424b8426fae1fb4873ddfafa4f1bddec13ec6a
Holayn <kwwong1597@gmail.com>
2020-09-26 14:13:45 -0400

    feat(vue): add username dropdown search

--- packages/chat-vue/src/views/Messages.vue
+++ packages/chat-vue/src/views/Messages.vue
@@ -2,12 +2,30 @@
   <div class="messages-grid h-full">
     <div class="sidebar flex flex-col bg-gray-700">
       <div class="flex justify-end pt-3 pr-3">
-        <button
-          class="inline-block text-sm leading-none border rounded text-white border-white hover:bg-opacity-50 hover:bg-white mt-4 lg:mt-0"
-          >
-          <!-- https://iconsvg.xyz/ -->
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
-        </button>
+        <div class="relative">
+          <button
+            class="relative z-50 inline-block text-sm leading-none border rounded text-white border-white hover:bg-opacity-50 hover:bg-white mt-4 lg:mt-0"
+            @click="toggleUserSearch()"
+            >
+            <!-- https://iconsvg.xyz/ -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+          </button>
+          <button
+            v-if="isUserSearchOpen"
+            class="fixed inset-0 h-full w-full bg-black opacity-50 cursor-default"
+            @click="toggleUserSearch()"
+          ></button>
+          <div class="absolute">
+            <div class="flex p-3 space-x-3 bg-gray-900 rounded-lg" v-if="isUserSearchOpen">
+              <input ref="userSearchInput" class="shadow appearance-none rounded py-2 px-3 text-white bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" v-model="userSearchInput" placeholder="username">
+              <button
+                class="inline-block text-sm px-4 py-2 mt-4 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-500 hover:bg-white lg:mt-0"
+                @click="search()">
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="flex-grow space-y-3 p-3">
         <div v-if="isLoadingSessions" class="flex h-full w-full">
@@ -42,7 +60,7 @@
     </div>
     <div class="input flex items-center px-2 bg-gray-900">
       <div class="flex-auto pr-2">
-        <input class="shadow appearance-none rounded w-full py-2 px-3 text-white bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="message" type="text" v-model="message" placeholder="Message">
+        <input class="shadow appearance-none rounded w-full py-2 px-3 text-white bg-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" v-model="message" placeholder="Message">
       </div>
       <div>
         <button
@@ -53,53 +71,6 @@
       </div>
     </div>
   </div>
-  <!-- <v-container fluid>
-    <v-row
-      justify="center"
-    >
-      <v-col cols="4" class="text-center">
-        <h1>Messages</h1>
-      </v-col>
-    </v-row>
-    <v-row class="messages">
-      <v-col cols="3" class="sessions-section">
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th class="text-center">Friends <div v-if="isLoadingSessions">
-                  loading
-                </div></th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr class="session-card" v-for="(session, i) in sessions" :key="i" @click="selectSession(session)">
-                <td class="text-left"> {{ displayUsersInSession(session) }} <span v-if="!session.read">*</span> </td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-col>
-      <v-col cols="8" class="chat-section">
-        <div v-if="isLoadingChats">
-          loading
-        </div>
-        <div class="chat-card" v-for="chat in chats" :key="chat.chatId" :style="{
-          color: chatColor(chat.sent),
-        }">
-          {{displayName(chat.userId)}} at {{new Date(new Number(chat.timestamp))}}: {{chat.message}}
-        </div>
-      </v-col>
-    </v-row>
-    <input v-model="message"/>
-    <button @click="sendMessage()">Send</button>
-    <div>
-      Start a new chat
-      <div>
-        Search for user: <input v-model="userSearchInput"/> <button @click="search()">Search</button>
-      </div>
-    </div>
-  </v-container> -->
 </template>
 
 <script lang="ts">
@@ -126,6 +97,7 @@ export default class extends Vue {
   // State
   private isLoadingSessions: boolean = false;
   private isLoadingChats: boolean = false;
+  private isUserSearchOpen: boolean = false;
 
   created() {
     window.addEventListener('keyup', this.onEnterPressed);
@@ -183,12 +155,14 @@ export default class extends Vue {
 
     if (sessionWithUser) {
       this.selectSession(sessionWithUser);
+      this.toggleUserSearch();
       return;
     }
 
     const newSession = Session.createSession('regular', this.$store.getters.user.userId, [user]);
     this.$store.dispatch('addSession', newSession);
     this.selectSession(newSession);
+    this.toggleUserSearch();
   }
 
   private async selectSession(session: ISession) {
@@ -244,8 +218,24 @@ export default class extends Vue {
 
   private onEnterPressed(key: KeyboardEvent) {
     if (key.code === 'Enter') {
+      if (this.isUserSearchOpen) {
+        this.search();
+        return;
+      }
       this.sendMessage();
     }
+  }
+
+  private toggleUserSearch() {
+    this.isUserSearchOpen = !this.isUserSearchOpen;
+    if (this.isUserSearchOpen) {
+      this.$nextTick(() => {
+        (this.$refs.userSearchInput as HTMLElement).focus();
+      });
+      return;
+    }
+
+    this.userSearchInput = '';
   }
 }
 </script>

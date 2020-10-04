11f0c0c8a285f97afdf824f03fd0906e49ca41c1
Holayn <kwwong1597@gmail.com>
2020-10-04 16:41:20 -0400

    feat(vue): show username in toolbar

--- packages/chat-vue/src/components/Toolbar.vue
+++ packages/chat-vue/src/components/Toolbar.vue
@@ -15,6 +15,9 @@
         </router-link>
       </div>
       <div>
+        <span>
+          {{username}}
+        </span>
         <a
           href="#"
           class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-orange-500 hover:bg-white mt-4 lg:mt-0"
@@ -31,6 +34,9 @@ import {Component, Vue} from 'vue-property-decorator';
 
 @Component({})
 export default class Toolbar extends Vue {
+  get username() {
+    return this.$store.getters.user.username;
+  }
   private logout() {
     this.$store.dispatch('logout');
   }
